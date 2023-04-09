46fcfd18c118cbaef1fbb31f84ac607a921c939d
jaycethanks <jaycethanks@outlook.com>
2023-04-09 20:45:13 +0800

    feat: 增加banner jayce's blog 字样

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,9 +1,7 @@
 import Heading from '@/components/common/Heading';
-import Font_Ba from '@/fonts/SvgIconFonts/Font_Ba';
-import Font_Dong from '@/fonts/SvgIconFonts/Font_Dong';
-import Font_Mu from '@/fonts/SvgIconFonts/Font_Mu';
-import Font_You from '@/fonts/SvgIconFonts/Font_You';
-
+import styles from "./banner.module.css" 
+import JaycesBlog from "./jaycesblog"
+import SpacerBar from '../common/SpacerBar';
 type BannerProps = {
   whetherData: any;
 };
@@ -13,34 +11,18 @@ const Banner: React.FC<StandardProps & BannerProps> = ({
 }) => {
   const { city, data } = whetherData;
   return (
-    <>
+    <div className={styles.fontancient}>
       <div
-        className="flex justify-between border-b border-dashed dark:border-gray-600"
+        className="flex flex-col h-14 sm:h-24 justify-center items-center border-b border-dashed dark:border-gray-600 text-6xl"
         {...rest}
       >
-        {/* hello */}
-        <div className="item flex flex-1 flex-col items-center sm:flex-row sm:items-start">
-          <Font_Mu
-            size="w-24 h-24 sm:w-24 sm:h-24 "
-            className="fill-TEXT_MAIN dark:fill-DARK_TEXT_MAIN"
-          ></Font_Mu>
-          <Font_Dong
-            size="w-24 h-24 sm:w-24 sm:h-24"
-            className="fill-TEXT_MAIN dark:fill-DARK_TEXT_MAIN"
-          ></Font_Dong>
-          <Font_You
-            size="w-24 h-24 sm:w-24 sm:h-24"
-            className="fill-TEXT_MAIN dark:fill-DARK_TEXT_MAIN"
-          ></Font_You>
-          <Font_Ba
-            size="w-24 h-24 sm:w-24 sm:h-24"
-            className="fill-TEXT_MAIN dark:fill-DARK_TEXT_MAIN"
-          ></Font_Ba>
-
-          {/* <Heading level={4}>Find me in below links !</Heading> */}
-        </div>
+        <JaycesBlog
+          size="w-auto h-full"
+          className="fill-TEXT_MAIN dark:fill-DARK_TEXT_MAIN"
+        ></JaycesBlog>
+        <SpacerBar gap={6}/>
         {/* whether */}
-        <div className="flex flex-col items-end">
+        {/* <div className="flex flex-col items-end">
           <ul className="flex flex-col flex-nowrap items-center justify-between gap-1 whitespace-nowrap  sm:flex-row">
             {data.map(
               (
@@ -78,9 +60,9 @@ const Banner: React.FC<StandardProps & BannerProps> = ({
             <span>2022 年 12 月 23 日</span>{" "}
             <span className="hidden sm:block">·</span> <span>{city}</span>
           </span>
-        </div>
+        </div> */}
       </div>
-    </>
+    </div>
   );
 };
 
