f447722a0d5a0facc940d317ab607332bfcfb9b9
jaycethanks <jaycethanks@outlook.com>
2023-08-06 22:22:39 +0800

    feat: optimize the size of build with minus the fonts

--- fonts/index.ts
+++ fonts/index.ts
@@ -1,32 +1,32 @@
 import localFont from '@next/font/local';
 
-const SongJianTi = localFont({ src: '../fonts/FangZhengShuSongJianTi.ttf' });
-const HeiTi = localFont({ src: '../fonts/FangZhengHeiTiJianTi.ttf' });
-const KaiTi = localFont({ src: '../fonts/FangZhengKaiTiJianTi.ttf' });
-const SourceHanSerifCN = localFont({ src: '../fonts/SourceHanSerifCN-Regular.otf' });
-const BorelRegular = localFont({ src: '../fonts/BorelRegular.ttf' });
-const SourceHanSansCNRegular = localFont({
-  src: '../fonts/SourceHanSansCN-Regular.otf',
-});
-const CaveatVariableFont = localFont({
-  src: '../fonts/CaveatVariableFont.ttf',
-});
-const VT323 = localFont({
-  src: '../fonts/VT323.ttf',
-});
+// const SongJianTi = localFont({ src: '../fonts/FangZhengShuSongJianTi.ttf' });
+// const HeiTi = localFont({ src: '../fonts/FangZhengHeiTiJianTi.ttf' });
+// const KaiTi = localFont({ src: '../fonts/FangZhengKaiTiJianTi.ttf' });
+// const SourceHanSerifCN = localFont({ src: '../fonts/SourceHanSerifCN-Regular.otf' });
+// const BorelRegular = localFont({ src: '../fonts/BorelRegular.ttf' });
+// const SourceHanSansCNRegular = localFont({
+//   src: '../fonts/SourceHanSansCN-Regular.otf',
+// });
+// const CaveatVariableFont = localFont({
+//   src: '../fonts/CaveatVariableFont.ttf',
+// });
+// const VT323 = localFont({
+//   src: '../fonts/VT323.ttf',
+// });
 const JetBrainsMonoVariableFont = localFont({
   src: '../fonts/JetBrainsMonoVariableFont.ttf',
 });
 const Fonts = {
-  SongJianTi: SongJianTi.className,
-  HeiTi: HeiTi.className,
-  KaiTi: KaiTi.className,
-  SourceHanSansCNRegular: SourceHanSansCNRegular.className,
-  SourceHanSerifCN: SourceHanSerifCN.className,
-  BorelRegular: BorelRegular.className,
-  CaveatVariableFont: CaveatVariableFont.className,
+  // SongJianTi: SongJianTi.className,
+  // HeiTi: HeiTi.className,
+  // KaiTi: KaiTi.className,
+  // SourceHanSansCNRegular: SourceHanSansCNRegular.className,
+  // SourceHanSerifCN: SourceHanSerifCN.className,
+  // BorelRegular: BorelRegular.className,
+  // CaveatVariableFont: CaveatVariableFont.className,
   JetBrainsMonoVariableFont: JetBrainsMonoVariableFont.className,
-  VT323: VT323.className,
+  // VT323: VT323.className,
 
 };
 
